4edced0c87d3d1334af5f7908aa7943333bfded2
karine <76127656+karineML@users.noreply.github.com>
2023-07-28 13:53:43 -0400

    update code
    update code

--- rl_jitai_simulation/utils.py
+++ rl_jitai_simulation/utils.py
@@ -28,29 +28,31 @@ def run_RL_loop_DQN(env, agent, n_episodes, b_train, b_plot=False, plot_title=''
     plt.ylim(y_lim); plt.xlabel('episode'); plt.ylabel('return'); plt.grid(); plt.show()
   return return_values
 
-def run_RL_loop_REINFORCE(env, agent, n_episodes, b_train, b_plot=False, plot_title='', y_lim=(-200,3500), color='C0'):
+def run_RL_loop_REINFORCE(env, agent, M, n_episodes, b_train, b_plot=False, plot_title='', y_lim=(-200,3500)):
   return_values = []
   for i in tqdm(range(n_episodes), desc ="REINFORCE {} σ={} duration".format(env.chosen_obs_names_str, env.sigma)):
     if b_train: agent.init_grad()
-    obs = env.reset()
-    return_sample = 0
-    done = False
-    while not done:
-      action = agent.choose_action(obs)
-      obs_, reward, done, info = env.step(action)
-      agent.store_rewards(reward)
-      obs = obs_
-      return_sample += reward
-    if b_train: agent.compute_grad()
+    return_samples = []
+    for m in range(M):
+      obs = env.reset()
+      return_sample = 0
+      done = False
+      while not done:
+        action = agent.choose_action(obs)
+        obs_, reward, done, info = env.step(action)
+        agent.store_rewards(reward)
+        obs = obs_
+        return_sample += reward
+      return_samples.append(return_sample)
+      if b_train: agent.compute_grad()
     if b_train: agent.take_step(env.max_episode_length)
-    return_values.append(return_sample)
+    return_values.append(np.mean(return_samples))
   if b_plot:
     plt.figure(figsize=(3,2))
-    plt.plot(return_values, color=color)
+    plt.plot(return_values)
     if len(plot_title) < 1:
       plot_title = ' (σ={})'.format(env.sigma)
-    plot_detail = 'train' if b_train else 'perf'
-    plt.title('REINFORCE {} {}{}'.format(env.chosen_obs_names_str, plot_detail, plot_title))
+    plt.title('REINFORCE {} learning{}'.format(env.chosen_obs_names_str, plot_title))
     plt.ylim(y_lim); plt.xlabel('episode'); plt.ylabel('return'); plt.grid(); plt.show()
   return return_values
 
